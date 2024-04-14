1a2d7b612cafcfba093e7cdb4ce436ce928b99d7
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-04-14 10:12:03 -0400

    Update cn_to_en_format.py

--- docs/source/000900-Chinese-to-English-Format/cn_to_en_format.py
+++ docs/source/000900-Chinese-to-English-Format/cn_to_en_format.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """
+这是一个将中文文本转化为英文格式的脚本.
+
 1. 把中文标点符号转换为英文标点符号.
 2. 把中文和英文之间加上空格.
 3. 在一些 "([<,." 等标点符号的前后视情况加上空格.
+
+How to use:
+
+翻到本脚本最下面, 将你的文本内容写在 main 函数的 text 变量中, 然后运行脚本即可. 它会自动
+将输出的文本拷贝到剪贴板, 你只需 Ctrl + V 复制既可.
+
+Requirements::
+
+    pyperclip>=1.8.2,<2.0.0
 """
 
 import string
@@ -105,8 +116,17 @@ def main(text: str) -> str:
     return new_text
 
 
-if __name__ == "__main__":
-    text = """
-    W-2表格，又叫做年度工资总结表，W-2表格是雇主需要在每个报税年结束之后发给每个雇员和美国国家税务局Internal Revenue Service(IRS)的报税文件。W-2表格报告了员工的年薪和工资中扣缴的各类税款（联邦税、州税、地方税等）。W-2是非常非常非常重要的报税文件，里面的信息是填写报税表的关键！
-    """.strip()
-    main(text)
+# enter your text here
+text = """
+W-2表格，又叫做年度工资总结表，W-2表格是雇主需要在每个报税年结束之后发给每个雇员和美国国家税务局Internal Revenue Service(IRS)的报税文件。W-2表格报告了员工的年薪和工资中扣缴的各类税款（联邦税、州税、地方税等）。W-2是非常非常非常重要的报税文件，里面的信息是填写报税表的关键！
+""".strip()
+new_text = main(text)
+
+try:
+    import pyperclip
+
+    pyperclip.copy(new_text)
+    print("output copied to clipboard! You can paste it now.")
+except ImportError:
+    pass
+
