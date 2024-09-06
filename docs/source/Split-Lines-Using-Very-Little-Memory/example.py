e288a50d66dd2a668bf98933ca9769ef21dbdd45
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-09-06 12:53:34 -0400

    c

--- docs/source/Split-Lines-Using-Very-Little-Memory/example.py
+++ docs/source/Split-Lines-Using-Very-Little-Memory/example.py
@@ -6,6 +6,7 @@
 import io
 import uuid
 import gzip
+import time
 from pathlib import Path
 
 import polars as pl
@@ -19,10 +20,11 @@ path_source = dir_tmp.joinpath("source.json.gz")
 
 def make_source():
     """
-    一共 1M 行, 每个 uuid 重复 10 次. 压缩前 300MB, 压缩后 26.4MB. 最终的输出是 100 个 3MB 的小文件.
+    一共 1M 行, 每个 uuid 重复 100 次 (一条记录大约 3KB). 压缩前 3200MB, 压缩后 34.8MB.
+    最终的输出是 100 个 32MB 的小文件.
     """
     n_rows = 1_000_000
-    data = [{"id": i, "text": uuid.uuid4().hex * 10} for i in range(1, 1 + n_rows)]
+    data = [{"id": i, "text": uuid.uuid4().hex * 100} for i in range(1, 1 + n_rows)]
     df = pl.DataFrame(data)
     buffer = io.BytesIO()
     df.write_ndjson(buffer)
@@ -32,14 +34,16 @@ def make_source():
 @profile
 def main1():
     """
+    elapsed = 3.99
+
     Line #    Mem usage    Increment  Occurrences   Line Contents
     =============================================================
-    31     64.3 MiB     64.3 MiB           1   @profile
-    32                                         def main1():
-    33   1491.3 MiB   1427.0 MiB           1       lines = gzip.decompress(path_source.read_bytes()).decode("utf-8").splitlines()
-    34   1499.3 MiB      7.2 MiB         101       for ith, lst in enumerate(batched(lines, 10000), start=1):
-    35   1499.3 MiB      0.0 MiB         100           p = dir_tmp.joinpath(f"{str(ith).zfill(4)}.json")
-    36   1499.3 MiB      0.8 MiB         100           p.write_text("\n".join(lst))
+    34     63.3 MiB     63.3 MiB           1   @profile
+    35                                         def main1():
+    46   4531.5 MiB   4468.2 MiB           1       lines = gzip.decompress(path_source.read_bytes()).decode("utf-8").splitlines()
+    47   4562.5 MiB -11951.4 MiB         101       for ith, lst in enumerate(batched(lines, 10000), start=1):
+    48   4562.5 MiB  -8820.7 MiB         100           p = dir_tmp.joinpath(f"{str(ith).zfill(4)}.json")
+    49   4593.3 MiB  -8759.2 MiB         100           p.write_text("\n".join(lst))
     """
     lines = gzip.decompress(path_source.read_bytes()).decode("utf-8").splitlines()
     for ith, lst in enumerate(batched(lines, 10000), start=1):
@@ -50,14 +54,16 @@ def main1():
 @profile
 def main2():
     """
+    elapsed = 2.2
+
     Line #    Mem usage    Increment  Occurrences   Line Contents
     =============================================================
-    49     63.3 MiB     63.3 MiB           1   @profile
-    50                                         def main2():
-    51    770.5 MiB    707.1 MiB           1       buffer = io.BytesIO(gzip.decompress(path_source.read_bytes()))
-    52   1155.3 MiB     14.8 MiB         101       for ith, lst in enumerate(batched(buffer.readlines(), 10000), start=1):
-    53   1155.3 MiB      0.0 MiB         100           p = dir_tmp.joinpath(f"{str(ith).zfill(4)}.json")
-    54   1155.3 MiB      4.0 MiB         100           p.write_bytes(b"".join(lst))
+    52     62.7 MiB     62.7 MiB           1   @profile
+    53                                         def main2():
+    64   3543.1 MiB   3480.4 MiB           1       buffer = io.BytesIO(gzip.decompress(path_source.read_bytes()))
+    65   6985.7 MiB     80.7 MiB         101       for ith, lst in enumerate(batched(buffer.readlines(), 10000), start=1):
+    66   6985.7 MiB      0.0 MiB         100           p = dir_tmp.joinpath(f"{str(ith).zfill(4)}.json")
+    67   6985.7 MiB      1.9 MiB         100           p.write_bytes(b"".join(lst))
     """
     buffer = io.BytesIO(gzip.decompress(path_source.read_bytes()))
     for ith, lst in enumerate(batched(buffer.readlines(), 10000), start=1):
@@ -66,7 +72,10 @@ def main2():
 
 
 if __name__ == "__main__":
-    """ """
-    make_source()
-    # main1()
+    st = time.process_time()
+    # make_source()
+    main1()
     # main2()
+    et = time.process_time()
+    elapse = et - st
+    print(f"{elapse = }")
